ac1398af9c942888850ea8c3bf6e35a99f440bdf
Luis Henrique <senseyluiz@gmail.com>
2021-12-09 15:52:30 -0300

    Exercicio de fixaçao

---
+++ Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/fixacao1.js
@@ -0,0 +1,9 @@
+const acordar = () => 'Acordando!!!';
+const cafe = () => "Bora tomar café!!!";
+const dormir = () => 'Partiu dormir!!!';
+
+const doingThings = (func) => func();
+
+
+
+console.log(doingThings(dormir));
