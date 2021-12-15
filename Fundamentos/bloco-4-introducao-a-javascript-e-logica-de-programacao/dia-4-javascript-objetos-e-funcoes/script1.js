85c63e46e14e9f7a8350b7ade67158ee9b797944
Luis Henrique <senseyluiz@gmail.com>
2021-12-15 09:17:59 -0300

    Exercicio 5

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script1.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script1.js
@@ -44,3 +44,31 @@ console.log('----------------------///----------------------');
 for (key in info) {
   console.log(info[key]);
 }
+
+console.log('----------------------///----------------------');
+
+
+//* 5 -  Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+console.log(info.personagem + " e " + info2.personagem);
+console.log(`${info.origem} e ${info2.origem}`);
+console.log(`${info.nota} e ${info2.nota}`);
+
+if (info.recorrente === "Sim" && info2.recorrente === "Sim") {
+  console.log("Ambos recorrente.");
+}
+
+
+console.log('----------------------///----------------------');
