69f5d629c419f483e0f45f759a15d9866342b700
Luis Henrique <senseyluiz@gmail.com>
2021-12-15 10:25:47 -0300

    Bonus 6

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -137,6 +137,16 @@ console.log('Exercício 6');
 // Valor de teste: N = 5 .
 // Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
 
+function somatorio(n) {
+  let soma = 0;
+  for (let i = 0; i <= n; i += 1) {
+    soma += i;
+  }
+  return soma;
+}
+
+console.log(somatorio(5));
+
 
 console.log('-----------------------////-----------------------');
 
