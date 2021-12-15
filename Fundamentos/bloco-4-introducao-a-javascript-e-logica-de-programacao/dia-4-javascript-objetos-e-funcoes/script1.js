c7b9daef48bd2a3ae2c1a1c884bdcac6cad6c6db
Luis Henrique <senseyluiz@gmail.com>
2021-12-15 09:24:41 -0300

    Exercicio 6

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script1.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/script1.js
@@ -72,3 +72,21 @@ if (info.recorrente === "Sim" && info2.recorrente === "Sim") {
 
 
 console.log('----------------------///----------------------');
+
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [{
+    titulo: 'O Pior Dia de Todos',
+    autor: 'Daniela Kopsch',
+    editora: 'Tordesilhas',
+  }, ],
+};
+
+//* 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}`);
+
+
+console.log('----------------------///----------------------');
