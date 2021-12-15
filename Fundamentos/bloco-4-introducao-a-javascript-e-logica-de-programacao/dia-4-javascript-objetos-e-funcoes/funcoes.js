2c1b385cc678c81ae3a9ccaaebd3f9982c0319e9
Luis Henrique <senseyluiz@gmail.com>
2021-12-15 09:54:46 -0300

    Bonus 3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -30,7 +30,7 @@ console.log('-----------------------////-----------------------');
 
 
 
-console.log('Exercício 1');
+console.log('Exercício 2');
 
 // * 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 // Array de teste: [2, 3, 6, 7, 10, 1]; .
@@ -48,5 +48,28 @@ function maiorValor(arr) {
   return index;
 }
 console.log(maiorValor([2, 3, 6, 7, 10, 1]));
+
+console.log('-----------------------////-----------------------');
+
+
+
+console.log('Exercício 3');
+
+// * 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+// Valor esperado no retorno da função: 6 .
+
+function maiorValor(arr) {
+  let menor = arr[0];
+  let index = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] < menor) {
+      menor = arr[i];
+      index = i;
+    }
+  }
+  return index;
+}
+console.log(maiorValor([2, 4, 6, 7, 10, 0, -3]));
 
 console.log('-----------------------////-----------------------');
