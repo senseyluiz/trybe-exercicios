cb0bbc79545c4183cba6fba2bd52c9f7c2884f8a
Luis Henrique <senseyluiz@gmail.com>
2021-12-15 14:04:58 -0300

    Parte 2 - Exercicio 6

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -92,5 +92,7 @@ contentRight.style.margin = 'auto';
 
 // 5 - Troque a cor de fundo do elemento pai da section criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
 
+let contentCenter = document.querySelector('.center-content');
+contentCenter.style.backgroundColor = 'green';
 
 // 6 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
