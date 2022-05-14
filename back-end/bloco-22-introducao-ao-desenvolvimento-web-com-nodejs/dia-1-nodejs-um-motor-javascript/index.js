eae8d8ba3601f6583146661d8179d581ead26b07
Luis Henrique <senseyluiz@gmail.com>
2022-05-14 09:50:13 -0300

    Exercicios 4

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
@@ -1,8 +1,8 @@
 const calculaimc = require('./imc');
 const readline = require('readline-sync');
 
-const peso = readline.question('Digite seu peso: ');
-const altura = readline.question('Digite sua altura: ');
+const peso = readline.questionInt('Digite seu peso: ');
+const altura = readline.questionFloat('Digite sua altura: ');
 
 const imc = calculaimc(peso, altura);
 console.log(imc);
