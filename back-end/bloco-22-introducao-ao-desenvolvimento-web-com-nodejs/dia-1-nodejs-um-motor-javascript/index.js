93aece7984a392b7d0e2a646a184aa9dea779edf
Luis Henrique <senseyluiz@gmail.com>
2022-05-14 09:58:58 -0300

    Exercicios 5

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
@@ -5,4 +5,31 @@ const peso = readline.questionInt('Digite seu peso: ');
 const altura = readline.questionFloat('Digite sua altura: ');
 
 const imc = calculaimc(peso, altura);
-console.log(imc);
+let mensagem = ""
+
+switch (true) {
+  case imc < 18,5:
+    mensagem = `ÌMC: ${imc}: Abaixo do peso (magreza)`
+    break;
+
+  case imc < 25:
+    mensagem = `ÌMC: ${imc}: Peso normal`
+    break;
+
+  case imc < 30:
+    mensagem = `ÌMC: ${imc}: Sobrepeso`
+    break;
+
+  case imc < 35:
+    mensagem = `ÌMC: ${imc}: Obesidade grau I`
+    break;
+
+  case imc < 40:
+    mensagem = `ÌMC: ${imc}: Obesidade grau II`
+    break;
+
+  default:
+    mensagem = `ÌMC: ${imc}: Obesidade grau III e IV`
+    break;
+}
+console.log(mensagem);
